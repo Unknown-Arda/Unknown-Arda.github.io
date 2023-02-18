3ef46f57d5b2ddfef4f1576427a19353afc6bd98
Unknown-Arda <125037095+Unknown-Arda@users.noreply.github.com>
2023-02-18 18:36:38 +0100

    Add files via upload

--- 1.2.2/Javascript/shopFS.js
+++ 1.2.2/Javascript/shopFS.js
@@ -1,19 +1,19 @@
 // Shop [ Select | white skin ]
 function whiteSkin() {
-    document.getElementById("player").style.backgroundImage = "url(../Desgins/Skins/whiteSkin.png)";
+    document.getElementById("player").style.backgroundImage = "url(./Desgins/Skins/whiteSkin.png)";
   }
   
   // Shop [ Buy / Select | blue skin ]
   var ownBlue = false;
   function blueSkin() {
     if (ownBlue == false && gold > 9) {
-      document.getElementById("player").style.backgroundImage = "url(../Desgins/Skins/blueSkin.png)";
+      document.getElementById("player").style.backgroundImage = "url(./Desgins/Skins/blueSkin.png)";
       document.getElementById("blue").innerHTML = "OWNED";
       gold -= 10;
       ownBlue = true;
     }
     if (ownBlue == true) {
-      document.getElementById("player").style.backgroundImage = "url(../Desgins/Skins/blueSkin.png)";
+      document.getElementById("player").style.backgroundImage = "url(./Desgins/Skins/blueSkin.png)";
     }
   }
   
@@ -21,13 +21,13 @@ function whiteSkin() {
   var ownPink = false;
   function pinkSkin() {
     if (ownPink == false && gold > 9) {
-      document.getElementById("player").style.backgroundImage = "url(../Desgins/Skins/pinkSkin.png)";
+      document.getElementById("player").style.backgroundImage = "url(./Desgins/Skins/pinkSkin.png)";
       document.getElementById("pink").innerHTML = "OWNED";
       gold -= 10;
       ownPink = true;
     }
     if (ownPink == true) {
-      document.getElementById("player").style.backgroundImage = "url(../Desgins/Skins/pinkSkin.png)";
+      document.getElementById("player").style.backgroundImage = "url(./Desgins/Skins/pinkSkin.png)";
     }
   }
   
@@ -35,13 +35,13 @@ function whiteSkin() {
   var ownRed = false;
   function redSkin() {
     if (ownRed == false && gold > 9) {
-      document.getElementById("player").style.backgroundImage = "url(../Desgins/Skins/redSkin.png)";
+      document.getElementById("player").style.backgroundImage = "url(./Desgins/Skins/redSkin.png)";
       document.getElementById("red").innerHTML = "OWNED";
       gold -= 10;
       ownRed = true;
     }
     if (ownRed == true) {
-      document.getElementById("player").style.backgroundImage = "url(../Desgins/Skins/redSkin.png)";
+      document.getElementById("player").style.backgroundImage = "url(./Desgins/Skins/redSkin.png)";
     }
   }
   
@@ -49,12 +49,12 @@ function whiteSkin() {
   var ownDog = false;
   function dogSkin() {
     if (ownDog == false && gold > 99) {
-      document.getElementById("player").style.backgroundImage = "url(../Designs/Skins/dogSkin.png)";
+      document.getElementById("player").style.backgroundImage = "url(./Designs/Skins/dogSkin.png)";
       document.getElementById("dog").innerHTML = "OWNED";
       gold -= 100;
       ownDog = true;
     }
     if (ownDog == true) {
-      document.getElementById("player").style.backgroundImage = "url(../Designs/Skins/dogSkin.png)";
+      document.getElementById("player").style.backgroundImage = "url(./Designs/Skins/dogSkin.png)";
     }
   }
